da11de8c938cd5c719460dd4bf7b758f1722095c
roshk <roshka.dev@gmail.com>
2020-02-25 19:04:50 +0100

    refactoring

--- app/src/main/java/com/example/randoexpress/views/SignInFragment.kt
+++ app/src/main/java/com/example/randoexpress/views/SignInFragment.kt
@@ -1,4 +1,4 @@
-package com.example.randoexpress
+package com.example.randoexpress.views
 
 
 import android.os.Bundle
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.randoexpress.R
 
 /**
  * A simple [Fragment] subclass.
