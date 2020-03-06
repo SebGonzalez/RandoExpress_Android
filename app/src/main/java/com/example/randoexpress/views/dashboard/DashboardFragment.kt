39cd273e03c86c1912c54cf2e37bf7c67358327a
roshk <roshka.dev@gmail.com>
2020-03-06 22:39:31 +0100

    viewpager2 logic implemented

--- app/src/main/java/com/example/randoexpress/views/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/randoexpress/views/dashboard/DashboardFragment.kt
@@ -5,11 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.randoexpress.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class DashboardFragment : Fragment() {
+
+    private lateinit var demoCollectionAdapter: DemoCollectionAdapter
+    private lateinit var viewPager: ViewPager2
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,17 +30,48 @@ class DashboardFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val logInButton: Button = view.findViewById(R.id.login_button_temp)
-        val signInButton: Button = view.findViewById(R.id.signin_button_temp)
-        logInButton.setOnClickListener { v ->
-            Navigation
-                .findNavController(v)
-                .navigate(R.id.action_navigation_dashboard_to_loginFragment)
+        demoCollectionAdapter = DemoCollectionAdapter(this)
+        viewPager = view.findViewById(R.id.pager)
+        viewPager.adapter = demoCollectionAdapter
+        val tabLayout = view.findViewById(R.id.tab_layout) as TabLayout
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
+    }
+
+    class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+        override fun getItemCount(): Int = 2
+
+        override fun createFragment(position: Int): Fragment {
+            // Return a NEW fragment instance in createFragment(int)
+            val fragment = DemoObjectFragment()
+            fragment.arguments = Bundle().apply {
+                // Our object is just an integer :-P
+                putInt("object", position + 1)
+            }
+            return fragment
         }
-        signInButton.setOnClickListener { v ->
-            Navigation
-                .findNavController(v)
-                .navigate(R.id.action_navigation_dashboard_to_signInFragment)
+    }
+
+
+    // Instances of this class are fragments representing a single
+    // object in our collection.
+    class DemoObjectFragment : Fragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View {
+            return inflater.inflate(R.layout.attendee_item, container, false)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            arguments?.takeIf { it.containsKey("object") }?.apply {
+                val textView: TextView = view.findViewById(R.id.attendee_item_name)
+                textView.text = getInt("object").toString()
+            }
         }
     }
 }
